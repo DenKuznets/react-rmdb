4e04b3009d80741c5a8410de1e81e98921564c6f
Denis Kuznetsov workPC <denis.kyznecov@gmail.com>
2023-01-23 13:27:55 +0300

    fixed rmdbogo path

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import RMDBLogo from '../../images/react-movie.logo.svg';
+import RMDBLogo from '../../images/react-movie-logo.svg';
 import TMDBLogo from '../../images/tmdb_logo.svg';
 
 import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
