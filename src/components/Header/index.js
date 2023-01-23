06888b1d16032dd3715d649cc2d9ffa99a2c409d
Denis Kuznetsov workPC <denis.kyznecov@gmail.com>
2023-01-23 13:18:08 +0300

    header index.js imports

---
+++ src/components/Header/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import RMDBLogo from '../../images/react-movie.logo.svg';
+import TBDBLogo from '../../images/tmdb_logo.svg';
+
+import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
+
