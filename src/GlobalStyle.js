6fe8623433adf16f27d6f05a031d7f687c416107
Denis Kuznetsov workPC <denis.kyznecov@gmail.com>
2023-01-23 11:46:40 +0300

    global styles done

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -16,8 +16,27 @@ ${"" /* переменные как в обычном css */}
 
   * {
     box-sizing: border-box;
-    font-family: 'Abel';
-    
+    font-family: 'Abel', sans-serif;    
   }
 
+  body {
+    margin: 0;
+    padding: 0;
+
+    h1 {
+      font-size: 2rem;
+      font-weight: 600;
+      color: var(--white);      
+    }
+
+    h3 {
+      font-size: 1.1rem;
+      font-weight: 600;
+    }
+
+    p {
+      font-style: 1rem;
+      color: var(--white);
+    }
+  }
 `;
